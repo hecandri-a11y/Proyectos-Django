4c1f235dd98bc60d1e0ce102d93a7553713c999a
Hector <hecandri@gmail.com>
2026-06-15 22:06:52 -0400

    modificación de enlaces

--- instituto/alumnos/urls.py
+++ instituto/alumnos/urls.py
@@ -2,11 +2,40 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
 
-path('', views.index, name='index'),
+    # Categorías
+    path('arte/', views.arte, name='arte'),
+    path('deporte/', views.deporte, name='deporte'),
 
+    # Talentos de arte
+    path('dibujo/', views.dibujo, name='dibujo'),
+    path('pintura/', views.pintura, name='pintura'),
+    path('escultura/', views.escultura, name='escultura'),
 
+    # Talentos de deporte
+    path('futbol/', views.futbol, name='futbol'),
+    path('basquetbol/', views.basquetbol, name='basquetbol'),
+    path('natacion/', views.natacion, name='natacion'),
 
+    # Formularios
+    path('contacto/', views.Contacto, name='Contacto'),
+    path('formulariodeprueba/', views.formulariodeprueba, name='formulariodeprueba'),
+    path('localstore/', views.localstore, name='localstore'),
 
-
+    # Fichas individuales
+    path('AnaTorres/', views.AnaTorres, name='AnaTorres'),
+    path('AntonioVirgolini/', views.AntonioVirgolini, name='AntonioVirgolini'),
+    path('CarlosSoto/', views.CarlosSoto, name='CarlosSoto'),
+    path('CristobalSancho/', views.CristobalSancho, name='CristobalSancho'),
+    path('EnzoSanchez/', views.EnzoSanchez, name='EnzoSanchez'),
+    path('GuillermoGomez/', views.GuillermoGomez, name='GuillermoGomez'),
+    path('JuanMartinez/', views.JuanMartinez, name='JuanMartinez'),
+    path('JuanPerez/', views.JuanPerez, name='JuanPerez'),
+    path('MarcusAnton/', views.MarcusAnton, name='MarcusAnton'),
+    path('MariaLopez/', views.MariaLopez, name='MariaLopez'),
+    path('MartinAlejandro/', views.MartinAlejandro, name='MartinAlejandro'),
+    path('MatildaAlejandra/', views.MatildaAlejandra, name='MatildaAlejandra'),
+    path('SelenaMichell/', views.SelenaMichell, name='SelenaMichell'),
+    path('VictoriaPerez/', views.VictoriaPerez, name='VictoriaPerez'),
 ]
--- instituto/alumnos/views.py
+++ instituto/alumnos/views.py
@@ -6,3 +6,78 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'alumnos/index.html')
+
+def arte(request):
+    return render(request, 'html/arte.html')
+
+def deporte(request):
+    return render(request, 'html/deporte.html')
+
+def dibujo(request):
+    return render(request, 'html/dibujo.html')
+
+def pintura(request):
+    return render(request, 'html/pintura.html')
+
+def escultura(request):
+    return render(request, 'html/escultura.html')
+
+def futbol(request):
+    return render(request, 'html/futbol.html')
+
+def basquetbol(request):
+    return render(request, 'html/basquetbol.html')
+
+def natacion(request):
+    return render(request, 'html/natacion.html')
+
+def Contacto(request):
+    return render(request, 'html/Contacto.html')
+
+def formulariodeprueba(request):
+    return render(request, 'html/formulariodeprueba.html')
+
+def localstore(request):
+    return render(request, 'html/localstore.html')
+
+def AnaTorres(request):
+    return render(request, 'html/AnaTorres.html')
+
+def AntonioVirgolini(request):
+    return render(request, 'html/AntonioVirgolini.html')
+
+def CarlosSoto(request):
+    return render(request, 'html/CarlosSoto.html')
+
+def CristobalSancho(request):
+    return render(request, 'html/CristobalSancho.html')
+
+def EnzoSanchez(request):
+    return render(request, 'html/EnzoSanchez.html')
+
+def GuillermoGomez(request):
+    return render(request, 'html/GuillermoGomez.html')
+
+def JuanMartinez(request):
+    return render(request, 'html/JuanMartinez.html')
+
+def JuanPerez(request):
+    return render(request, 'html/JuanPerez.html')
+
+def MarcusAnton(request):
+    return render(request, 'html/MarcusAnton.html')
+
+def MariaLopez(request):
+    return render(request, 'html/MariaLopez.html')
+
+def MartinAlejandro(request):
+    return render(request, 'html/MartinAlejandro.html')
+
+def MatildaAlejandra(request):
+    return render(request, 'html/MatildaAlejandra.html')
+
+def SelenaMichell(request):
+    return render(request, 'html/SelenaMichell.html')
+
+def VictoriaPerez(request):
+    return render(request, 'html/VictoriaPerez.html')
